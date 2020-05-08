31e1c2ad08d130781a4891d761bad84854f3c69f
Andrei Appelt <andrei.appelt@gmail.com>
2020-05-08 13:29:11 -0300

    datatable contrato

--- src/views/Contrato.vue
+++ src/views/Contrato.vue
@@ -1,10 +1,107 @@
 <template>
   <v-row class="fill-height">
     <template>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :search="search"
+        :single-select="singleSelect"
+        item-key="name"
+        class="elevation-1"
+        show-select
+        multi-sort
+      >
+        <template v-slot:top>
+          <v-toolbar flat color="white">
+            <v-toolbar-title>Contratos</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Buscar"
+              single-line
+              hide-details
+            ></v-text-field>
+
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="70%">
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark class="mb-2" v-on="on"
+                  >Cadastrar</v-btn
+                >
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">{{ formTitle }}</span>
+                </v-card-title>
+
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.name"
+                          label="Dessert name"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.calories"
+                          label="Calories"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.fat"
+                          label="Fat (g)"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <v-text-field
+                          v-model="editedItem.carbs"
+                          label="Carbs (g)"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <v-text-field
+                          v-model="editedItem.protein"
+                          label="Protein (g)"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close"
+                    >Cancelar</v-btn
+                  >
+                  <v-btn color="blue darken-1" text @click="save">Salvar</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item)">
+            mdi-delete
+          </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize">Reset</v-btn>
+        </template>
+      </v-data-table>
+    </template>
+
+    <!-- <template>
       <v-card>
         <v-card-title>
-          <!-- Selecione os serviços
-          <v-spacer></v-spacer> -->
+         
           <v-text-field
             v-model="search"
             append-icon="mdi-magnify"
@@ -14,26 +111,29 @@
           ></v-text-field>
         </v-card-title>
         <template>
-          <div>
-            <v-data-table
-              :headers="headers"
-              :items="desserts"
-              :search="search"
-              :single-select="singleSelect"
-              item-key="name"
-              show-select
-            >
-              <template v-slot:item.selecionado="{ item }">
-                <v-simple-checkbox
-                  v-model="item.selecionado"
-                  disabled
-                ></v-simple-checkbox>
-              </template>
-            </v-data-table>
-          </div>
+          <v-row class="fill-height">
+            <div>
+              <v-data-table
+                :headers="headers"
+                :items="desserts"
+                :search="search"
+                :single-select="singleSelect"
+                item-key="name"
+                show-select
+                multi-sort
+              >
+                <template v-slot:item.selecionado="{ item }">
+                  <v-simple-checkbox
+                    v-model="item.selecionado"
+                    disabled
+                  ></v-simple-checkbox>
+                </template>
+              </v-data-table>
+            </div>
+          </v-row>
         </template>
       </v-card>
-    </template>
+    </template> -->
 
     <!-- Lista Padrao com painel expansivo -->
     <!-- <v-row justify="center">
@@ -79,7 +179,7 @@
 
         <!-- <v-system-bar></v-system-bar> -->
         <v-toolbar flat>
-          <v-toolbar-title>Cadastrar Contrato</v-toolbar-title>
+          <v-toolbar-title>Novo Contrato</v-toolbar-title>
           <v-spacer></v-spacer>
           <!-- <div>
             <v-switch
@@ -218,100 +318,147 @@ export default {
       { text: "Fat (g)", value: "fat" },
       { text: "Carbs (g)", value: "carbs" },
       { text: "Protein (g)", value: "protein" },
-      { text: "Iron (%)", value: "iron" },
+      { text: "Actions", value: "actions", sortable: false },
     ],
-    desserts: [
-      {
-        name: "Frozen Yogurt",
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: "1%",
-        selecionado: false,
-      },
-      {
-        name: "Ice cream sandwich",
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        iron: "1%",
-        selecionado: false,
-      },
-      {
-        name: "Eclair",
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        iron: "7%",
-        selecionado: false,
-      },
-      {
-        name: "Cupcake",
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        iron: "8%",
-      },
-      {
-        name: "Gingerbread",
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        iron: "16%",
-        selecionado: false,
-      },
-      {
-        name: "Jelly bean",
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        iron: "0%",
-        selecionado: false,
-      },
-      {
-        name: "Lollipop",
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        iron: "2%",
-        selecionado: false,
-      },
-      {
-        name: "Honeycomb",
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        iron: "45%",
-        selecionado: false,
-      },
-      {
-        name: "Donut",
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-        iron: "22%",
-        selecionado: false,
-      },
-      {
-        name: "KitKat",
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7,
-        iron: "6%",
-        selecionado: false,
-      },
-    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+    defaultItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
   }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "Novo Item" : "Editar Item";
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      this.desserts = [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+        },
+        {
+          name: "Jelly bean",
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+        },
+        {
+          name: "Lollipop",
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+        },
+        {
+          name: "Honeycomb",
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+        },
+        {
+          name: "Donut",
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+        },
+        {
+          name: "KitKat",
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+        },
+      ];
+    },
+
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      const index = this.desserts.indexOf(item);
+      confirm("Are you sure you want to delete this item?") &&
+        this.desserts.splice(index, 1);
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+      } else {
+        this.desserts.push(this.editedItem);
+      }
+      this.close();
+    },
+  },
 };
 </script>
 
